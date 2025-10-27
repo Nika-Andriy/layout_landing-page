44b3504038834a5ba640122462fab8c9b6151874
Andriy <andriit35@gmail.com>
2025-10-27 17:07:53 +0200

    Fix suggested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const phoneIcon = document.querySelector('.icon--phone');
+const number = document.querySelector('.icon__number');
+const topBar = document.querySelector('.top-bar');
+
+phoneIcon.addEventListener('mouseenter', () => {
+  number.classList.add('is-visible');
+});
+
+topBar.addEventListener('mouseleave', () => {
+  number.classList.remove('is-visible');
+});
